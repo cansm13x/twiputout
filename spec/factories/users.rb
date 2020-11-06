f1f5cdc944ba295afd1f2c1b0448eaa4b375f0de
cansm13x <cansm.13x@gmail.com>
2020-11-06 22:56:25 +0900

    userモデルの単体テストコード

---
+++ spec/factories/users.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :user do
+    user_name              {"username"}
+    email                  {Faker::Internet.free_email}
+    password               {"12345k"}
+    password_confirmation  {password}
+  end
+end
