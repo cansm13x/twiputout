01c83e7b71b9ab47884a5916957b5f576525be3a
cansm13x <cansm.13x@gmail.com>
2020-11-07 15:17:47 +0900

    Active Storageの導入

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,4 +1,10 @@
 class MemosController < ApplicationController
   def index
   end
+
+  private
+
+  def memo_params
+    params.require(:memo).permit(:memo_text, :image).marge(user_id: current_user.id)
+  end
 end
