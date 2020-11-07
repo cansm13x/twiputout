4f83ce5d270a71df30b0a2770574e4e3b24359a1
cansm13x <cansm.13x@gmail.com>
2020-11-07 17:59:13 +0900

    Rubocopを実行

--- spec/factories/memos.rb
+++ spec/factories/memos.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :memo do
-    memo_text   {Faker::Lorem.sentence}
-    image       {Faker::Lorem.sentence}
+    memo_text   { Faker::Lorem.sentence }
+    image       { Faker::Lorem.sentence }
     association :user
   end
 end
--- spec/models/memo_spec.rb
+++ spec/models/memo_spec.rb
@@ -7,25 +7,25 @@ RSpec.describe Memo, type: :model do
     end
 
     context 'メモ投稿がうまくいくとき' do
-      it "memo_textとimageが存在すれば投稿できる" do
+      it 'memo_textとimageが存在すれば投稿できる' do
         expect(@memo).to be_valid
       end
       it 'memo_textのみあれば投稿できる' do
-        @memo.image = ""
+        @memo.image = ''
         expect(@memo).to be_valid
       end
     end
 
     context 'メモ投稿がうまくいかないとき' do
       it 'memo_textがないと投稿できない' do
-        @memo.memo_text = ""
+        @memo.memo_text = ''
         @memo.valid?
         expect(@memo.errors.full_messages).to include("Memo text can't be blank")
       end
       it 'ユーザーが紐づいていないと投稿できない' do
         @memo.user = nil
         @memo.valid?
-        expect(@memo.errors.full_messages).to include("User must exist")
+        expect(@memo.errors.full_messages).to include('User must exist')
       end
     end
   end
