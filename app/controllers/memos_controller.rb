a92679aaa5690bd49297a71b2d9791171e6de956
cansm13x <cansm.13x@gmail.com>
2020-11-07 16:40:58 +0900

    投稿したメモの情報をテーブルに保存

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -8,9 +8,18 @@ class MemosController < ApplicationController
     @memo = Memo.new
   end
 
+  def create
+    @memo = Memo.new(memo_params)
+    if @memo.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
   private
 
   def memo_params
-    params.require(:memo).permit(:memo_text, :image).marge(user_id: current_user.id)
+    params.require(:memo).permit(:memo_text, :image).merge(user_id: current_user.id)
   end
 end
