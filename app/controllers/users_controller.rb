eb6c3754dbfc11f2d1266a146e310417dc675082
cansm13x <cansm.13x@gmail.com>
2020-11-08 22:07:51 +0900

    未ログイン状態で直接URLを入力するとログインページに遷移する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
+  before_action :authenticate_user!, only: [:show, :edit, :update]
   before_action :set_user, only: [:show]
   before_action :redirect_root, only: [:show]
 
