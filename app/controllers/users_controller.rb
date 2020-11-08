11c8db971f55f3e878059325696aaffebf961018
cansm13x <cansm.13x@gmail.com>
2020-11-08 21:15:00 +0900

    ログイン状態でも違うユーザーのマイページのURLを直接入力するとトップページに遷移する

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!, only: [:show]
   before_action :set_user, only: [:show]
+  before_action :redirect_root, only: [:show]
 
   def show
   end
-  
+
   private
 
   def user_params
@@ -13,5 +14,9 @@ class UsersController < ApplicationController
 
   def set_user
     @user = User.find(params[:id])
+  end
+
+  def redirect_root
+    redirect_to root_path unless current_user.id == @user.id
   end
 end
