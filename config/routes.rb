d43766ea24fdb252518bde6fcfa0a1c6140c4c99
cansm13x <cansm.13x@gmail.com>
2020-11-08 22:04:38 +0900

    editアクションとupdateアクションのルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "memos#index"
   resources :memos, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :users, only: [:show]
+  resources :users, only: [:show, :edit, :update]
 end
