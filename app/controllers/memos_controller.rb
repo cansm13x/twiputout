1b288c9036ae3b26f7126af12b3e681ff94ebd52
cansm13x <cansm.13x@gmail.com>
2020-11-08 15:58:22 +0900

    updateアクションを定義

--- app/controllers/memos_controller.rb
+++ app/controllers/memos_controller.rb
@@ -1,5 +1,6 @@
 class MemosController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
+  before_action :set_item, only: [:edit, :update]
 
   def index
     @memos = Memo.includes(:user).order('created_at DESC')
@@ -19,12 +20,23 @@ class MemosController < ApplicationController
   end
 
   def edit
-    @memo = Memo.find(params[:id])
+  end
+
+  def update
+    if @memo.update(memo_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
   private
 
   def memo_params
     params.require(:memo).permit(:memo_text, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @memo = Memo.find(params[:id])
   end
 end
