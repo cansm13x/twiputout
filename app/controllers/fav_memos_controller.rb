fa23a4df4365bfbb1b82f73da5211354b6b5e950
cansm13x <cansm.13x@gmail.com>
2020-11-11 17:36:53 +0900

    fav_memosコントローラーにcreateとdestroyアクションの処理を記述

--- app/controllers/fav_memos_controller.rb
+++ app/controllers/fav_memos_controller.rb
@@ -1,2 +1,22 @@
 class FavMemosController < ApplicationController
+  before_action :authenticate_user!, only: [:create, :destroy]
+  before_action :set_memo, only: [:create, :destroy]
+
+  def create
+    fav_memo = current_user.fav_memos.build(memo_id: params[:memo_id])
+    fav_memo.save
+    redirect_to root_path
+  end
+
+  def destroy
+    fav_memo = FavMemo.find_by(memo_id: params[:memo_id], user_id: current_user.id)
+    fav_memo.destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def set_memo
+    @memo = Memo.find(params[:memo_id])
+  end
 end
