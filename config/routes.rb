548b91594ef41679fb65b28b3a8299f50395d2ba
cansm13x <cansm.13x@gmail.com>
2020-11-11 17:35:52 +0900

    fav_memoのcreateとdestroyをルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations' }
   get "users/show" => "users#show"
   root to: "memos#index"
-  resources :memos, only: [:index, :new, :create, :edit, :update, :destroy]
+  resources :memos, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resource :fav_memos, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 end
